41a5acff6987126fbd86a82f81e1415bd0749a59
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-06-15 01:45:16 -0300

    simplifying loading

--- src/one.c
+++ src/one.c
@@ -2,14 +2,10 @@
 
 #include "imports/imports.fdefine.h"
 
-void start_bear_props(LuaCEmbed *l){
-    LuaCEmbed_set_string_lib_prop(l, "nil_code","nil");
-    LuaCEmbed_add_callback(l, "fetch", private_lua_bear_fetch);
-}
 
 int load_lua_bear(lua_State *state){
     LuaCEmbed * l  = newLuaCEmbedLib(state);
-    start_bear_props(l);
-
+    LuaCEmbed_set_string_lib_prop(l, "nil_code","nil");
+    LuaCEmbed_add_callback(l, "fetch", private_lua_bear_fetch);
     return LuaCembed_perform(l);
 }
