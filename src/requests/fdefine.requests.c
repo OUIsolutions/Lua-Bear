eae67e51c542c9fad4eec651333be2c30f0dc022
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-06-15 01:04:46 -0300

    att

--- src/requests/fdefine.requests.c
+++ src/requests/fdefine.requests.c
@@ -61,15 +61,19 @@ LuaCEmbedResponse *private_lua_bear_fetch(LuaCEmbed *args){
     }
 
 
- 
-
 
     if(LuaCEmbed_has_errors(args)){
         return LuaCEmbed_send_error(LuaCEmbed_get_error_message(args));
     }
-
+    BearHttpsResponse *response = BearHttpsRequest_fetch(request);
+    BearHttpsRequest_free(request);
+    if(BearHttpsResponse_error(response)){
+            char *error_msg  =  BearHttpsResponse_get_error_msg(response);
+            LuaCEmbedResponse *error_response = LuaCEmbed_send_error(error_msg);
+            BearHttpsResponse_free(response);
+            return error_response;
+    }
+   
     return NULL;
-
-
 
 }
