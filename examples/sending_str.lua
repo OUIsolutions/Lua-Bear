7d17a4f39ba5df6b548200843e1858e54766aacf
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-06-15 04:24:02 -0300

    att

--- examples/sending_str.lua
+++ examples/sending_str.lua
@@ -3,9 +3,6 @@ local luabear  = require("luaBear.luaBear")
 local response = luabear.fetch({
     url="http://localhost:3001/",
     method="POST",
-    headers={
-        username="your username",
-        password ="your password"
-    }
+    body="Hello, LuaBear!",
 })
 print(response.read_body())
